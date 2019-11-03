ce593bd72072696488e8b90e3fdb78785a725584
Borisenko_Daniil <danikbord@gmail.com>
2019-11-03 21:10:57 +0300

    Напишите код на C# для вычисления окружности, без изменения класса circle

--- addition_even_numbers/addition_even_numbers/Program.cs
+++ addition_even_numbers/addition_even_numbers/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using static System.Console;
 
 namespace addition_even_numbers
@@ -9,16 +10,34 @@ namespace addition_even_numbers
         static void Main(string[] args)
         {
             //Console.WriteLine("Hello World!");
-           Console.WriteLine("RESULT IS " + Solutions.additionEvenNumbers());
+            Console.WriteLine("RESULT IS " + Solutions.additionEvenNumbers());
         }
     }
 
     class Solutions
     {
         // Сумма всех четных чисел до какого-то предела
-        public static long additionEvenNumbers ( long privateNumber = 2)
+        public static long additionEvenNumbers(long privateNumber = 2)
         {
             return (privateNumber < 100 ? privateNumber += additionEvenNumbers(privateNumber + 2) : (privateNumber));
+        }
+
+        static long TotalAllEvenNumbers(int[] intArray)
+        {
+            return intArray.Where(i => i % 2 == 0).Sum(i => (long) i);
+        }
+        
+        static long TotalAllEvenNumbers2(int[] intArray) {
+            return (from i in intArray where i % 2 == 0 select (long)i).Sum();
+        }
+        
+        //Напишите код на C# для вычисления окружности, без изменения класса circle
+        public sealed class Circle {
+            private double radius;
+ 
+            public double Calculate(Func<double, double> op) {
+                return op(radius);
+            }
         }
     }
 }
