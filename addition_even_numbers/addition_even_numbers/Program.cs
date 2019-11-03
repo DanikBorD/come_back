a2daffa80244980ed4fa157260c0206e9834062c
Borisenko_Daniil <danikbord@gmail.com>
2019-11-03 16:41:21 +0300

    Сумма всех четных чисел до какого-то предела

---
+++ addition_even_numbers/addition_even_numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using static System.Console;
+
+namespace addition_even_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+           Console.WriteLine("RESULT IS " + Solutions.additionEvenNumbers());
+        }
+    }
+
+    class Solutions
+    {
+        // Сумма всех четных чисел до какого-то предела
+        public static long additionEvenNumbers ( long privateNumber = 2)
+        {
+            return (privateNumber < 100 ? privateNumber += additionEvenNumbers(privateNumber + 2) : (privateNumber));
+        }
+    }
+}
